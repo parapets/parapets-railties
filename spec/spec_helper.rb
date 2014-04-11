49ddea10e19494dc1e0c67263d984deb14695fb3
Seba Gamboa <me@sagmor.com>
2014-04-11 13:01:10 -0500

    Track coverage with code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 require "parapets/railties"
 
 Dir[File.expand_path("../support/*.rb", __FILE__)].each { |f| require f }
